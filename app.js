fbbf271e6f0f9e22467b298def522e4406f480bc
Nasrin Thaiba <nasrinthaiba3@gmail.com>
2026-05-21 10:15:47 +0530

    cors update

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const dns = require("node:dns").promises;
+dns.setServers(['1.1.1.1', '8.8.8.8']);
+
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
-const commentRouter = require("./routes/comments")
-const usersRouter = require("./routes/users")
+const commentRouter = require("./routes/comments");
+const usersRouter = require("./routes/users");
 
 const app = express();
 app.use(express.json());
@@ -16,20 +19,24 @@ const PORT = process.env.PORT;
 
 mongoose.connect(DB_URL)
 .then(() => console.log("MongoDB connected successfully!"))
-.catch((err) => console.log("MongoDB error", err))
+.catch((err) => console.log("MongoDB error", err));
 
 app.use(cors({
-    origin : ["https://frontendadminblogapp.netlify.app", "https://frontendpublicblogapp.netlify.app",  "http://localhost:5174"],
-    methods : ["GET", "POST", "PUT", "DELETE"],
-    credentials : true
+    origin: [
+        "https://frontendadminblogapp.netlify.app",
+        "https://frontendpublicblogapp.netlify.app",
+        "http://localhost:5174",
+        "http://localhost:5175"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
 }));
 
-app.use("/api/users", usersRouter)
+app.use("/api/users", usersRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running in the port : ${PORT} `)
-})
+    console.log(`Server is running in the port : ${PORT}`);
+});
